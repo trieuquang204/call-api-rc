b246ffbceb39ede33a779e121cd27ca6cd533b6e
trieuquang <nhuhexiyeuamnhac1997@gmail.com>
2020-09-09 22:32:11 +0700

    88 tich hop react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import Menu from './components/Menu/Menu';
 import ProductList from './components/ProductList/ProductList';
+import routes from './routes';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Menu/>
-
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12">
+      <Router>
+        <div>
+          <Menu />
+          <div className="container">
+            <div className="row">
+              {/* <div className="col-md-12">
               <button type="button" className="btn btn-info" style={{ marginBottom: '10px' }}>
                 Them san pham
               </button>
               <ProductList/>
+            </div> */}
+              {this.showContentMenus(routes)}
             </div>
           </div>
         </div>
-      </div>
+      </Router>
     )
+  }
+  showContentMenus = (routes) => {
+    var result = null;
+    if (routes.length > 0) {
+      result = routes.map((route, index) => {
+        return (
+          <Route
+            key={index}
+            path={route.path}
+            exact={route.exact}
+            component={route.main}
+          />
+        )
+      })
+    }
+    return <Switch>{result}</Switch>
   }
 }
 
---
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NotFoundPage extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="alert alert-warning">
+          <h1>Khong tim thay trang</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NotFoundPage;
